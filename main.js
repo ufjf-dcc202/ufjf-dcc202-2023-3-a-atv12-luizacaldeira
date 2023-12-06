829ebd2925ea6f43e1145ce3f4c809d8156b8bb1
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-12-06 20:28:57 -0300

    colocando a função como ouvinte

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 
-function leFormulario(){
+document.entrada.addEventListener('submit', leFormulario);
+
+function leFormulario(event){
+    event.preventDefault();
     const fruta = document.entrada.fruta.value;
     const quantidade = document.entrada.fruta.value;
 
