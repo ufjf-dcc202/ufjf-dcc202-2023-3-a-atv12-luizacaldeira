11efe3859b4f410c4650e027522578a03665f5bb
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-12-06 19:57:14 -0300

    Criando a variável estoque com os itens já criados

--- estoque.js
+++ estoque.js
@@ -1,9 +1,11 @@
 //estoque.js
+let estoque = {
 let joao = [
     {tipo:"maca", qtd: 1},
     {tipo:"pera", qtd: 2},
-];
+],
 let maria = [
     {tipo:"maca", qtd: 2},
     { tipo: "banana", qtd: 4}
-];
+],
+};
