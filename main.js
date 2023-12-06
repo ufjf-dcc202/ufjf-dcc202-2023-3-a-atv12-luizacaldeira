ae2669300e77e6461a57fce66bf3ad69730a1225
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-12-06 20:27:23 -0300

    criando a função leFormulario

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function leFormulario(){
+    const fruta = document.entrada.fruta.value;
+    const quantidade = document.entrada.fruta.value;
+
+    console.log (`fruta: ${fruta} qtd:${quantidade}`);
+}
