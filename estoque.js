77dc48b7b919819d67803980f59fe16792d073af
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-12-06 20:59:56 -0300

    Ajustes finais

--- estoque.js
+++ estoque.js
@@ -1,11 +1,16 @@
 //estoque.js
 let estoque = {
-let joao = [
-    {tipo:"maca", qtd: 1},
-    {tipo:"pera", qtd: 2},
+    'joao' : [
+        {tipo:"maca", qtd: 1},
+        {tipo:"pera", qtd: 2},
 ],
-let maria = [
-    {tipo:"maca", qtd: 2},
-    { tipo: "banana", qtd: 4}
+    'maria' : [
+        {tipo:"maca", qtd: 2},
+        { tipo: "banana", qtd: 4}
 ],
 };
+
+ export function getEstoque(){
+    return estoque;
+}
+
--- main.js
+++ main.js
@@ -4,7 +4,8 @@ document.entrada.addEventListener('submit', leFormulario);
 function leFormulario(event){
     event.preventDefault();
     const fruta = document.entrada.fruta.value;
-    const quantidade = document.entrada.fruta.value;
+    const quantidade = document.entrada.quantidade.value;
 
     console.log (`fruta: ${fruta} qtd:${quantidade}`);
+    //document.entrada.submit();
 }
