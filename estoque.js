98d75dfbeb0612f3ddd4d4d6340bdff603ec579f
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-12-13 20:16:24 -0300

    criando e exportando a função transação

--- estoque.js
+++ estoque.js
@@ -1,16 +1,22 @@
 //estoque.js
 let estoque = {
     'joao' : [
-        {tipo:"maca", qtd: 1},
-        {tipo:"pera", qtd: 2},
-],
+        {'tipo':"maca", 'qtd': 1},
+        {'tipo':"pera", 'qtd': 2},
+    ],
     'maria' : [
-        {tipo:"maca", qtd: 2},
-        { tipo: "banana", qtd: 4}
-],
+        {'tipo':"maca", 'qtd': 2},
+        { 'tipo': "banana", 'qtd': 4}
+    ]
 };
 
  export function getEstoque(){
-    return estoque;
+    return structuredClone(estoque);
 }
 
+ export function transacao(origem,destino,quantidade,fruta){
+    if(origem === 'pomar'){
+        estoque[destino][0].qtd += quantidade;
+    }
+ }
+//export {getEstoque}
